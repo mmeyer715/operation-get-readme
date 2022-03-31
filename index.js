31c080bd6e13aae398e0a0eab62d10d97896dd88
Maria Meyer <mbean1216@icloud.com>
2022-03-31 15:30:57 -0500

    creating questions

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your project title?'
+    }
+    // {
+    //   type: 'checkbox',
+    //   message: 'What languages do you know?',
+    //   name: 'stack',
+    //   choices: ['HTML', 'CSS', 'JavaScript', 'MySQL']
+    // },
+    // {
+    //   type: 'list',
+    //   message: 'What is your preferred method of communication?',
+    //   name: 'contact',
+    //   choices: ['email', 'phone', 'telekinesis']
+    // }
+  ])
+  .then(data => {
+    // const filename = `${data.name
+    //   .toLowerCase()
+    //   .split(' ')
+    //   .join('')}.json`;
+
+    // fs.writeFile(filename, JSON.stringify(data, null, '\t'), err =>
+    //   err ? console.log(err) : console.log('Success!')
+    console.log(data.name);
+    // );
+});
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +41,4 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+// init();
