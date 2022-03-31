3faa8e29031a44e4c9ba131ffc14d4295c5d24f4
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-03-31 16:00:58 -0500

    Create outLIneConstants file

---
+++ outlineConstants.js
@@ -0,0 +1,14 @@
+const OutlineConstants = {
+    TITLE: "# ",
+    DESCRIPTION: "## Description",
+    TABLE_OF_CONTENTS: "## Table of Contents",
+    INSTALLATION: "## Installation",
+    USAGE: "## Usage",
+    CREDITS: "## Credits",
+    LICENSE: "## License",
+    BADGES: "## Badges",
+    CONTRIBUTING: "## Contributing",
+    TESTS: "## Tests"
+}
+
+module.exports = OutlineConstants;
